5c60eb8f1e5316039605d676d3c99e054e4ec71a
Rafael Henrique <rafael@rah.com.br>
2022-01-04 20:25:57 -0300

    feat: :sparkles: Adds recipe creation for logged in users

--- recipe_manager/recipes/views.py
+++ recipe_manager/recipes/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 from ingredients.models import Ingredient, MeasurementUnits
 
@@ -24,6 +24,45 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
+
+
+@login_required
+def create(request):
+    all_ingredients = Ingredient.objects.all().order_by('name')
+    all_measurement_units = MeasurementUnits.choices
+
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+            ingredients = request.POST.getlist('ingredients')
+            parsed_ingredients = _parse_ingredients(ingredients)
+
+            recipe = Recipe()
+            recipe.name = name
+            recipe.save()
+
+            # create an instance of 'through' object for each relationship:
+            for one_ingredient in parsed_ingredients:
+                RecipeIngredient.objects.create(
+                    recipe=recipe,
+                    ingredient=Ingredient.objects.get(id=one_ingredient['id']),
+                    amount=one_ingredient['amount'],
+                    measurement_unit=one_ingredient['unit']
+                )
+
+            return redirect('recipes:detail', pk=recipe.id)
+
+        except Exception as e:
+            return render(request, 'recipes/create_form.html', {
+                'all_ingredients': all_ingredients,
+                'all_measurement_units': all_measurement_units,
+                'error': str(e),
+            })
+    else:
+        return render(request, 'recipes/create_form.html', {
+            'all_ingredients': all_ingredients,
+            'all_measurement_units': all_measurement_units,
+        })
 
 
 @login_required
@@ -61,7 +100,12 @@ def edit(request, pk):
             })
 
         except Exception as e:
-            return render(request, 'recipes/edit_form.html', {'recipe': recipe, 'error': str(e)})
+            return render(request, 'recipes/edit_form.html', {
+                'recipe': recipe,
+                'all_ingredients': all_ingredients,
+                'all_measurement_units': all_measurement_units,
+                'error': str(e),
+            })
     else:
         return render(request, 'recipes/edit_form.html', {
             'recipe': recipe,
