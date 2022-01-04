fb91503af5e21299377ca182afc9a85ccc71d55e
Rafael Henrique <rafael@rah.com.br>
2022-01-03 22:28:25 -0300

    feat: :construction: Recipe edition for authenticated users

--- recipe_manager/recipes/views.py
+++ recipe_manager/recipes/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from ingredients.models import Ingredient, MeasurementUnits
 
 from recipes.models import Recipe
 
@@ -19,3 +22,33 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
+
+
+@login_required
+def edit(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    all_ingredients = Ingredient.objects.all().order_by('name')
+    all_measurement_units = MeasurementUnits.choices
+
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+
+            recipe.name = name
+            recipe.save()
+
+            return render(request, 'recipes/edit_form.html', {
+                'recipe': recipe,
+                'all_ingredients': all_ingredients,
+                'all_measurement_units': all_measurement_units,
+                'success': 'The recipe was saved successfuly',
+            })
+
+        except Exception as e:
+            return render(request, 'recipes/edit_form.html', {'recipe': recipe, 'error': str(e)})
+    else:
+        return render(request, 'recipes/edit_form.html', {
+            'recipe': recipe,
+            'all_ingredients': all_ingredients,
+            'all_measurement_units': all_measurement_units,
+        })
