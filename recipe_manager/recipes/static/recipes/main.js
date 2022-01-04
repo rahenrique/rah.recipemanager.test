e614423e5464d67a49da1ad9b5667e6af7fb8227
Rafael Henrique <rafael@rah.com.br>
2022-01-04 19:33:33 -0300

    style: :art: Format code, clean-up CSS classes

--- recipe_manager/recipes/static/recipes/main.js
+++ recipe_manager/recipes/static/recipes/main.js
@@ -1,19 +1,32 @@
 $(function () {
 	const IngredientItem = ({ id, name, amount, unit }) => `
-		<tr>
+		<tr data-id="${id}">
 			<td>
 				<input type="hidden" name="ingredients" value='{"id":"${id}","amount":"${amount}","unit":"${unit}"}'>
 				${name}
 			</td>
-			<td class="text-end">${amount}</td>
-			<td>${unit}</td>
-			<td class="text-end"><em>&ndash; save recipe to calculate &ndash;</em></td>
+			<td class="text-end">${amount} ${unit}</td>
+			<td class="text-end"><small><em>save to calculate</em></small></td>
 			<td class="text-end"><button type="button" class="js_remove_ingredient btn btn-danger btn-sm">x</button></td>
-		</tr>
-		`;
+		</tr>`;
+
+	function disableUsedIngredients() {
+		$('#ingredients_input option[data-id]').attr('disabled', false);
+		$('#current_ingredients tbody tr').each(function () {
+			let optionElement = $('#ingredients_input option[data-id="' + $(this).data('id') + '"]');
+			optionElement.attr('disabled', true);
+		});
+	}
+
+	function resetAddIngredientForm() {
+		$('#amount_input').val('');
+		$('#ingredients_input').prop('selectedIndex', 0);
+		$('#measurement_input').prop('selectedIndex', 0);
+	}
 
 	$('#current_ingredients').on('click', 'button.js_remove_ingredient', function (event) {
 		$(event.target).closest('tr').hide('fast').remove();
+		disableUsedIngredients();
 	});
 
 	$('#js_add_ingredient').on('click', function (event) {
@@ -28,8 +41,11 @@ $(function () {
 		$('#current_ingredients tbody').append(
 			values.map(IngredientItem).join('')
 		);
+		disableUsedIngredients();
+		resetAddIngredientForm();
 	});
 
+	disableUsedIngredients();
 });
 
 
