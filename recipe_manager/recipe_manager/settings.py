9203b0bce85f1360d8f0332027935fc94ee2dcbc
Rafael Henrique <rafael@rah.com.br>
2022-04-26 11:39:57 -0300

    chore: :coffin: Remove static files from repository

--- recipe_manager/recipe_manager/settings.py
+++ recipe_manager/recipe_manager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k)i#8s2-k472n*jo3l#vj3m^&q=+423fs8=rfz(1$g!uf8r%_t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
